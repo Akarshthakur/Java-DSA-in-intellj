3645dfcf2cd82a9695d1371eb6a91d150e320ff0
Akarshthakur <161207674+Akarshthakur@users.noreply.github.com>
2024-08-19 08:35:41 +0530

    Add files via upload

---
+++ IdeaProjects/java1/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] acm){
+        Scanner scan= new Scanner(System.in);
+        System.out.print("enter day:");
+        String operator=scan.next();
+
+        int a=10;
+        int b=10;
+        switch(day){
+            case "+":
+            System.out.println(a+b);
+            break;
+            case "-":
+                System.out.println(a-b);
+                break;
+                default;
+        }
+    }
+}
---
+++ IdeaProjects/practise/src/IntroTopics/variablesDemo.java
@@ -0,0 +1,13 @@
+package IntroTopics;
+
+public class variablesDemo {
+    public static void main(String[] args) {
+
+        // Assigning the value during declaration.
+        int speedLimit = 70;
+        System.out.println(speedLimit);
+        int speed;
+        speed=20;
+        System.out.println(speed);
+    }
+}
